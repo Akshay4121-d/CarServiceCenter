3e5810ab28cae28a037b075a15d9b1704352f40b
Akshay4121-d <57613465+Akshay4121-d@users.noreply.github.com>
2019-12-01 10:27:42 -0200

    Carsparc
    abc

---
+++ src/app/bookingformaintenance/bookingformaintenance.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bookingformaintenance',
+  templateUrl: './bookingformaintenance.component.html',
+  styleUrls: ['./bookingformaintenance.component.css']
+})
+export class BookingformaintenanceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
